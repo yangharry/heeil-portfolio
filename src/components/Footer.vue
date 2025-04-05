25b05491b1c82c9950dee4095fee1f3a16c58322
yangharry <myid7771@naver.com>
2025-04-05 10:50:26 +0900

    refactor : convert script to TypeScript in Footer.vue and simplify link interface

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -34,18 +34,10 @@
       </div>
       <!-- 소셜 링크: 마진 적용, 중앙 정렬, 간격 설정 -->
       <div class="mt-3 sm:mt-4 flex justify-center space-x-3 sm:space-x-4">
-        <!-- GitHub 링크: 회색 색상, 호버 시 크기 변화 효과 -->
-        <a
-          v-for="link in socialLinks"
-          :key="link.name"
-          :href="link.url"
-          :target="link.external ? '_blank' : '_self'"
-          class="text-gray-300 transition-colors transform hover:scale-110 duration-200"
-        >
-          <component :is="link.isComponent ? link.icon : 'img'" v-if="!link.isSvg" :src="link.icon" :alt="link.name" class="h-5 w-5 sm:h-6 sm:w-6 fill-current text-white" />
-          <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 sm:h-6 sm:w-6" fill="currentColor" viewBox="0 0 24 24">
-            <path :d="link.svgPath" />
-          </svg>
+        <!-- 소셜 링크: 각 링크 순회 -->
+        <a v-for="link in socialLinks" :key="link.name" :href="link.url" target="_blank" class="text-gray-300 transition-colors transform hover:scale-110 duration-200">
+          <!-- 이미지가 있는 경우 -->
+          <img :src="link.iconSrc" :alt="link.name" class="h-5 w-5 sm:h-6 sm:w-6 fill-current text-white" />
         </a>
       </div>
     </div>
@@ -59,11 +51,7 @@ import { ref, computed } from 'vue';
 interface SocialLink {
   name: string;
   url: string;
-  external: boolean;
-  icon: string;
-  isSvg: boolean;
-  isComponent: boolean;
-  svgPath?: string;
+  iconSrc: string;
 }
 
 // 연락처 정보 타입 정의
@@ -88,21 +76,13 @@ const contactInfo = ref<ContactInfo>({
 const socialLinks = ref<SocialLink[]>([
   {
     name: 'GitHub',
-    url: '#',
-    external: false,
-    icon: '',
-    isSvg: true,
-    isComponent: false,
-    svgPath:
-      'M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z',
+    url: 'https://github.com/yangharry',
+    iconSrc: 'github-white-icon.svg',
   },
   {
     name: '티스토리',
     url: 'https://startatage30.tistory.com/',
-    external: true,
-    icon: '/tistory-logo.svg',
-    isSvg: false,
-    isComponent: false,
+    iconSrc: 'tistory-logo.svg',
   },
 ]);
 </script>
